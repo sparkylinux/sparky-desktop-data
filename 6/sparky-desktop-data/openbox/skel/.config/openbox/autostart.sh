1e06f84639589cbe2a16c191ee3c9dab8854f039
pavroo <pavroo@onet.eu>
2021-06-12 18:10:05 +0200

    20210612

--- 6/sparky-desktop-data/openbox/skel/.config/openbox/autostart.sh
+++ 6/sparky-desktop-data/openbox/skel/.config/openbox/autostart.sh
@@ -1,15 +1,25 @@
 #! /bin/bash
 
+if [ -f /usr/bin/sparky-polkit ]; then
 sparky-polkit &
+fi
+if [ -f /usr/bin/nitrogen ]; then
 nitrogen --restore && \
+fi
 if [ -f /usr/bin/tint2 ]; then
 tint2 &
 elif [ -f /usr/bin/fbpanel ]; then
 fbpanel &
 fi
+if [ -f /usr/bin/xscreensaver ]; then
 xscreensaver -nosplash &
+fi
+if [ -f /usr/bin/pnmixer ]; then
 (sleep 5; pnmixer) &
+fi
+if [ -f /usr/bin/thunar ]; then
 thunar --daemon &
+fi
 if [ -f /opt/sparky/nm-applet-reload ]; then
 /opt/sparky/nm-applet-reload &
 fi
